86e59748eeab6789b97dff10a45453f3d7d10f0b
Christopher Lambert <christopherwhb@gmail.com>
2022-01-02 20:27:11 +0000

    Resume

--- js/helpers.js
+++ js/helpers.js
@@ -7,5 +7,22 @@ function loadSections(){
     $("#portfolio").load("html/sections/sportifolio.html");
     $("#about").load("html/sections/sabout.html");
     $("#contact").load("html/sections/scontacts.html");
+    $("#resume").load("html/sections/sresume.html");
+}
+
+function printPdf (data , title ) {
+    const byteCharacters = atob ( data );
+    const byteNumbers = new Array ( byteCharacters.length );
+    for ( let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers [i] = byteCharacters.charCodeAt (i);
+    }
+    const byteArray = new Uint8Array ( byteNumbers );
+    const file = new Blob ([ byteArray ], { type: 'application/pdf;base64' }) ;
+    const fileURL = URL.createObjectURL ( file );
+    const winPDF = window.open ( fileURL , title );
+    // Open PDF
+    winPDF.onload = (() => {
+      setTimeout (() => winPDF.document.title = title , 1000) ;
+    }) ;
 }
 
